048e8aac3a6466a6e4cbd514976928c7cc823882
bdhay <buddhay@gmx.net>
2020-09-03 14:52:37 +0200

    kernel config not needed for Illusion-Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.oneplus_fajit
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := enchilada_defconfig
+#TARGET_KERNEL_CONFIG := enchilada_defconfig
 
 # inherit from the proprietary version
 -include vendor/oneplus/fajita/BoardConfigVendor.mk
